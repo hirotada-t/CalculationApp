7042fe827db4ffb96caaba645af93557774d7ec2
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-05-13 22:59:00 +0900

    値の表示・計算・クリア関数

--- calc.js
+++ calc.js
@@ -1,4 +1,21 @@
 var app = new Vue({
   el: "#app",
-  data: {}
+  data: {
+    input:"",
+    result:""
+  },
+  methods:{
+    addNum:function(e){
+      const num = e.currentTarget.innerText;
+      this.input += num;
+    },
+    calc:function(){
+      this.result = this.input;
+      this.input = "";
+    },
+    clearResult:function(){
+      this.result = "";
+      this.input = "";
+    }
+  }
 })
