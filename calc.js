4c511c180aac29c8d648f0b3b52a9ac19adf7aca
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-05-15 22:21:14 +0900

    数式の入力条件を追加

--- calc.js
+++ calc.js
@@ -1,21 +1,27 @@
 var app = new Vue({
   el: "#app",
   data: {
-    input:"",
+    formula:"",
     result:""
   },
   methods:{
     addNum:function(e){
-      const num = e.currentTarget.innerText;
-      this.input += num;
+      let input = e.currentTarget.innerText;
+      const latestFormula = this.formula[this.formula.length - 1]
+      if(this.formula == ""){
+        if(input == "+" || input == "*" || input =="/") input = "";// 先頭：number or minus
+      } else if(!Number(latestFormula)) {
+        if(input == "+" || input == "-" || input == "*" || input =="/") input = "";
+      }
+      this.formula += input;
     },
     calc:function(){
-      this.result = this.input;
-      this.input = "";
+      this.result = this.formula;
+      this.formula = "";
     },
     clearResult:function(){
       this.result = "";
-      this.input = "";
+      this.formula = "";
     }
   }
 })
