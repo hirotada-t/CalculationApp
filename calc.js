cd6c244d332f3d718acf7454747763c8488bed39
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-05-16 09:52:04 +0900

    feat: button disabled to avoid wrong input

--- calc.js
+++ calc.js
@@ -1,25 +1,27 @@
 var app = new Vue({
   el: "#app",
   data: {
-    formula:"",
-    result:""
+    formula: "",
+    result: "",
+    clicked: true
   },
-  methods:{
-    addNum:function(e){
+  methods: {
+    addNum: function (e) {
       let input = e.currentTarget.innerText;
-      const latestFormula = this.formula[this.formula.length - 1]
-      if(this.formula == ""){
-        if(input == "+" || input == "*" || input =="/") input = "";// 先頭：number or minus
-      } else if(!Number(latestFormula)) {
-        if(input == "+" || input == "-" || input == "*" || input =="/") input = "";
-      }
+      this.checkFormula();
       this.formula += input;
+      this.checkFormula();
     },
-    calc:function(){
+    checkFormula:function(){
+      const latestFormula = this.formula[this.formula.length - 1];
+      if (Number(latestFormula)) this.clicked = false;
+      else this.clicked = true;
+    },
+    calc: function () {
       this.result = this.formula;
       this.formula = "";
     },
-    clearResult:function(){
+    clearResult: function () {
       this.result = "";
       this.formula = "";
     }
