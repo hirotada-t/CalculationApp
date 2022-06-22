10b96f34dfccbd1c18ac256bc109df2eeb23e807
hiro <t.hir0tada.biz@gmail.com>
2022-06-22 15:27:25 +0900

    feat/styling

--- calc.js
+++ calc.js
@@ -1,13 +1,17 @@
 var app = new Vue({
   el: "#app",
+  
   data: {
     formula: "",
     lastInput: "",
     result: "",
+    nums:[9,8,7,6,5,4,3,2,1],
+    signs:["÷","×","－","＋"],
     clicked: true
   },
+
   methods: {
-    addNum: function (e) {
+    addNum(e) {
       let input = e.currentTarget.innerText;
       this.lastInput = this.formula[this.formula.length - 1];
       this.clicked = false;
@@ -18,18 +22,20 @@ var app = new Vue({
       if (!Number(input)) input = " " + input + " ";
       this.formula += input;
     },
-    checkFormula: function () {
+
+    checkFormula() {
       this.calc();
     },
-    calc: function () {
+
+    calc() {
       this.result = calcNum(this.formula);
       this.formula = "";
       this.clicked = true;
     },
-    bsFormula: function () {
+    bsFormula() {
 
     },
-    clearResult: function () {
+    clearResult() {
       this.result = "";
       this.formula = "";
       this.clicked = true;
@@ -41,30 +47,30 @@ function calcNum(str) {
   const formulaArr = str.split(" ");
   const temp = [];
   for (let i = 1; i < formulaArr.length; i += 2) {
-    if (formulaArr[i] == "*" || formulaArr[i] == "/") temp.push(i);
+    if (formulaArr[i] == "×" || formulaArr[i] == "÷") temp.push(i);
   }
   //[5,7]
   if (temp.length > 0) {
     for (let j = 0; j < temp.length; j++) {
-      if (formulaArr[temp[j]] == "*") {
+      if (formulaArr[temp[j]] == "×") {
         formulaArr[temp[j] + 1] = Number(formulaArr[temp[j] - 1]) * Number(formulaArr[temp[j] + 1]);
         formulaArr[temp[j] - 1] = 0;
-      } else if (formulaArr[temp[j]] == "/") {
+      } else if (formulaArr[temp[j]] == "÷") {
         formulaArr[temp[j] + 1] = Number(formulaArr[temp[j] - 1]) / Number(formulaArr[temp[j] + 1]);
         formulaArr[temp[j] - 1] = 0;
       }
     }
   }
   const rmZero = formulaArr.filter(f =>f !== 0);
-  const rmX = rmZero.filter(f =>f !== "*");
-  const rmSrash = rmX.filter(f =>f !== "/");
+  const rmX = rmZero.filter(f =>f !== "×");
+  const rmSrash = rmX.filter(f =>f !== "÷");
   console.log(rmSrash)
   for (let j = 1; j < rmSrash.length; j+= 2) {
     if(rmSrash[j + 1] == 0) continue;
-    if(rmSrash[j] == "+") {
+    if(rmSrash[j] == "＋") {
       rmSrash[j + 1] = Number(rmSrash[j - 1]) + Number(rmSrash[j + 1]);
       rmSrash[j - 1] = 0;
-    } else if(rmSrash[j] == "-") {
+    } else if(rmSrash[j] == "－") {
       rmSrash[j + 1] = Number(rmSrash[j - 1]) - Number(rmSrash[j + 1]);
       rmSrash[j - 1] = 0;
     } 
