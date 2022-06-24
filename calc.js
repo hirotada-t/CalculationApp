ffdf2e4248db3ec82300a632824674e74946748d
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-06-24 10:37:47 +0900

    feat/function/bs

--- calc.js
+++ calc.js
@@ -1,80 +1,125 @@
 var app = new Vue({
   el: "#app",
-  
+
   data: {
-    formula: "",
-    lastInput: "",
+    formulaArr: [],
+    formulaStr: "",
     result: "",
-    nums:[9,8,7,6,5,4,3,2,1],
-    signs:["÷","×","－","＋"],
+    nums: [9, 8, 7, 6, 5, 4, 3, 2, 1],
+    signs: ["÷", "×", "－", "＋"],
     clicked: true
   },
 
   methods: {
     addNum(e) {
       let input = e.currentTarget.innerText;
-      this.lastInput = this.formula[this.formula.length - 1];
-      this.clicked = false;
-
-      if (!Number(this.lastInput) && !Number(input)) {
-        this.formula = this.formula.slice(0, this.formula.length - 1);
+      if (this.formulaArr.length == 0) {
+        this.formulaArr.push(input);
+        this.formulaStr = this.formulaArr.join(" ");
+        this.clicked = false;
+      } else {
+        this.checkAddNum(input);
       }
-      if (!Number(input)) input = " " + input + " ";
-      this.formula += input;
+    },
+
+    checkAddNum(input) {
+      const prevInput = this.formulaArr[this.formulaArr.length - 1];
+      if (!Number(prevInput) && !Number(input)) {
+        this.formulaArr[this.formulaArr.length - 1] = input;
+      } else if (Number(prevInput) && Number(input)) {
+        this.formulaArr[this.formulaArr.length - 1] += input;
+      } else {
+        this.formulaArr.push(input);
+      }
+      this.formulaStr = this.formulaArr.join(" ");
     },
 
     checkFormula() {
+      // const leftParenthesesIndex = this.checkBrackets("(");
+      // const rightParenthesesIndex = this.checkBrackets(")");
+
+      // console.log(leftParenthesesIndex)
+      // console.log(rightParenthesesIndex)
+
       this.calc();
     },
 
-    calc() {
-      this.result = calcNum(this.formula);
-      this.formula = "";
-      this.clicked = true;
-    },
-    bsFormula() {
+    // checkBrackets(lr) {
+    //   const parenthesesIndex = [];
 
-    },
-    clearResult() {
-      this.result = "";
-      this.formula = "";
+    //   for (let i = 0; i < this.formulaArr.length; i++) {
+    //     if (this.formulaArr[i] === lr) parenthesesIndex.push(i);
+    //   }
+
+    //   return parenthesesIndex;
+    // },
+
+    // error() {
+    //   alert("数式が間違えています。");
+    // },
+
+    calc() {
+      this.result = this.calcNum;
+      this.formulaArr = [];
+      this.formulaStr = "";
       this.clicked = true;
+    },
+
+    bsFormula() {
+      this.formulaArr.splice(-1, 1);
+      this.formulaStr = this.formulaArr.join(" ");
+    },
+
+    clearResult() {
+      if (this.result == "") {
+        this.formulaArr = [];
+        this.formulaStr = "";
+      } else {
+        this.result = "";
+      }
+      this.clicked = true;
+    }
+  },
+
+  computed: {
+
+    calcNum() {
+      const temp = [];
+      // ×÷の位置を検索
+      for (let i = 1; i < this.formulaArr.length; i += 2) {
+        if (this.formulaArr[i] == "×" || this.formulaArr[i] == "÷") temp.push(i);
+      }
+
+      // 乗算・除算を実行
+      if (temp.length > 0) {
+        for (let j = 0; j < temp.length; j++) {
+          if (this.formulaArr[temp[j]] == "×") {
+            this.formulaArr[temp[j] + 1] = Number(this.formulaArr[temp[j] - 1]) * Number(this.formulaArr[temp[j] + 1]);
+            this.formulaArr[temp[j] - 1] = 0;
+          } else if (this.formulaArr[temp[j]] == "÷") {
+            this.formulaArr[temp[j] + 1] = Number(this.formulaArr[temp[j] - 1]) / Number(this.formulaArr[temp[j] + 1]);
+            this.formulaArr[temp[j] - 1] = 0;
+          }
+        }
+      }
+
+      // zero,✕,÷を除いた要素を抽出
+      const rmZero = this.formulaArr.filter(f => f !== 0);
+      const rmX = rmZero.filter(f => f !== "×");
+      const rmSrash = rmX.filter(f => f !== "÷");
+
+      // 加算・減算を実行
+      for (let j = 1; j < rmSrash.length; j += 2) {
+        if (rmSrash[j + 1] == 0) continue;
+        if (rmSrash[j] == "＋") {
+          rmSrash[j + 1] = Number(rmSrash[j - 1]) + Number(rmSrash[j + 1]);
+          rmSrash[j - 1] = 0;
+        } else if (rmSrash[j] == "－") {
+          rmSrash[j + 1] = Number(rmSrash[j - 1]) - Number(rmSrash[j + 1]);
+          rmSrash[j - 1] = 0;
+        }
+      }
+      return rmSrash[rmSrash.length - 1]
     }
   }
 })
-
-function calcNum(str) {
-  const formulaArr = str.split(" ");
-  const temp = [];
-  for (let i = 1; i < formulaArr.length; i += 2) {
-    if (formulaArr[i] == "×" || formulaArr[i] == "÷") temp.push(i);
-  }
-  //[5,7]
-  if (temp.length > 0) {
-    for (let j = 0; j < temp.length; j++) {
-      if (formulaArr[temp[j]] == "×") {
-        formulaArr[temp[j] + 1] = Number(formulaArr[temp[j] - 1]) * Number(formulaArr[temp[j] + 1]);
-        formulaArr[temp[j] - 1] = 0;
-      } else if (formulaArr[temp[j]] == "÷") {
-        formulaArr[temp[j] + 1] = Number(formulaArr[temp[j] - 1]) / Number(formulaArr[temp[j] + 1]);
-        formulaArr[temp[j] - 1] = 0;
-      }
-    }
-  }
-  const rmZero = formulaArr.filter(f =>f !== 0);
-  const rmX = rmZero.filter(f =>f !== "×");
-  const rmSrash = rmX.filter(f =>f !== "÷");
-  console.log(rmSrash)
-  for (let j = 1; j < rmSrash.length; j+= 2) {
-    if(rmSrash[j + 1] == 0) continue;
-    if(rmSrash[j] == "＋") {
-      rmSrash[j + 1] = Number(rmSrash[j - 1]) + Number(rmSrash[j + 1]);
-      rmSrash[j - 1] = 0;
-    } else if(rmSrash[j] == "－") {
-      rmSrash[j + 1] = Number(rmSrash[j - 1]) - Number(rmSrash[j + 1]);
-      rmSrash[j - 1] = 0;
-    } 
-  }
-
-  return rmSrash[rmSrash.length - 1]
-}
